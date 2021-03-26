fe9b07f792663e876f67f132bfcb56f8cd1976fd
DelPozoAmoAndres <asdfandres09@gmail.com>
2021-03-26 01:38:27 +0100

    SDI-2021-1007-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,9 +1,33 @@
-module.exports = function (app) {
+module.exports = function (app,swig) {
+    app.get("/canciones", function (req, res) {
+        let canciones = [{
+            "nombre" : "Blank space",
+            "precio" : "1.2"
+        },{
+            "nombre" : "See you again",
+            "precio" : "1.3"
+        },{
+            "nombre" : "Uptown Funk",
+            "precio" : "1.1"
+        }];
+        let respuesta = swig.renderFile('views/btienda.html',{
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+        });
+        res.send(respuesta);
+    });
+
     app.post("/cancion", function (req, res) {
        res.send("Canción agragada: "+req.body.nombre +"<br>"
                 + " genero: "+ req.body.genero +"<br>"
                 + " precio: "+ req.body.precio);
     });
+    app.get('/canciones/agregar', function(req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
 
     app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
