6dd7db960e0d3abde5a101e51a1a03e6ab3cbe41
DelPozoAmoAndres <asdfandres09@gmail.com>
2021-03-26 01:01:18 +0100

    SDI-2021-1007-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,11 +1,8 @@
 module.exports = function (app) {
-    app.get("/canciones", function (req, res) {
-        let respuesta = "";
-        if (req.query.nombre != null)
-            respuesta = 'Nombre: ' + req.query.nombre + '<br>';
-        if (typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: ' + req.query.autor;
-        res.send(respuesta);
+    app.post("/cancion", function (req, res) {
+       res.send("Canción agragada: "+req.body.nombre +"<br>"
+                + " genero: "+ req.body.genero +"<br>"
+                + " precio: "+ req.body.precio);
     });
 
     app.get('/suma', function (req, res) {
@@ -20,5 +17,4 @@ module.exports = function (app) {
         let respuesta = 'id: ' + req.params.id + '<br>' + 'Género: ' + req.params.genero;
         res.send(respuesta);
     });
-
 };
