1998bf1190b810fdf33adaf6f808418d3cdba25c
DelPozoAmoAndres <asdfandres09@gmail.com>
2021-04-01 14:14:35 +0200

    SDI-2021-1007-8.5-Colecciones relacionadas y routers.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Modulos
-let express=require("express");
+let express = require("express");
 let app = express();
 
 let expressSession = require('express-session');
@@ -12,28 +12,62 @@ app.use(expressSession({
 let crypto = require('crypto');
 let fileUpload = require('express-fileupload');
 app.use(fileUpload());
-let mongo =require('mongodb');
-let swig  = require('swig');
+let mongo = require('mongodb');
+let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 let gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+gestorBD.init(app, mongo);
+
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) {
+// dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : " + req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+
+//routerAudios
+let routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {"_id": mongo.ObjectID(idCancion) }, function (canciones) {
+            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerAudios
+app.use("/audios/",routerAudios);
 
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
-app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.ciqqc.mongodb.net:27017,tiendamusica-shard-00-01.ciqqc.mongodb.net:27017,tiendamusica-shard-00-02.ciqqc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-it5h9m-shard-0&authSource=admin&retryWrites=true&w=majority');
-app.set('clave','abcdefg');
-app.set('crypto',crypto);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.ciqqc.mongodb.net:27017,tiendamusica-shard-00-01.ciqqc.mongodb.net:27017,tiendamusica-shard-00-02.ciqqc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-it5h9m-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app,swig,gestorBD);  // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app,swig,gestorBD);  // (app, param1, param2, etc.)
-require("./routes/rautores.js")(app,swig);  // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD);  // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD);  // (app, param1, param2, etc.)
+require("./routes/rautores.js")(app, swig);  // (app, param1, param2, etc.)
 
 //Lanzar el servidor
-app.listen(app.get('port'),function (){
+app.listen(app.get('port'), function () {
     console.log("Servidor activo");
 });
